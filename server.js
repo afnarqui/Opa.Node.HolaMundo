f02c0e1e1f91b941dd6db91ef8ffb671230da560
afnarqui <afnarqui@hotmail.com>
2015-07-03 23:41:49 -0500

    server.js
    es la entrada principal de la aplicación el cual llama al servidor
    creado con el framework express

---
+++ server.js
@@ -0,0 +1,19 @@
+
+var http = require('http'),
+	config = require('./config.json'),
+	expressServer = require('./app/expressServer.js');
+
+
+var app = new expressServer();
+
+var server = http.createServer(app.expressServer);
+
+server.listen(5000);
+
+
+
+
+
+
+
+
